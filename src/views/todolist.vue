cf01bc0610e5ecdae977349f4789596d026e4ceb
user <li162002@mail.ru>
2021-04-18 14:56:16 +0300

    data

---
+++ src/views/todolist.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <table>
+            <tr>        
+                <td>telo</td>
+                <td> Is planet?</td>
+                <td>date and Avtor</td>
+            </tr>
+            <todoitem
+            v-for="todo of todos"
+            v-bind:todo="todo"
+            :key="todo"/>
+        </table>
+    </div>
+</template>
+<script>
+import todoitem from '@/views/todoitem'
+export default {
+    props:['todos'],
+    components:{
+        todoitem
+    },
+    methods:{
+
+    }
+}
+
+</script>
