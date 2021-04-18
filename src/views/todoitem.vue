67df151d2833169113017b97f910b8d89ea16393
user <li162002@mail.ru>
2021-04-18 14:55:56 +0300

    data

---
+++ src/views/todoitem.vue
@@ -0,0 +1,18 @@
+<template>
+<tr>
+    <td>{{todo.name}}</td>
+    <td>{{todo.isPlanet}}</td>
+    <td>{{todo.discoveryDate}} {{todo.discoveredBy}}
+    </td>
+</tr>
+</template>
+<script>
+export default {
+    props: {
+        todo:{
+            type:Object,
+            required:true
+        }
+    }
+}
+</script>
