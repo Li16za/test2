089e0adbd51382b15ac0832a0ce376da20c76d54
user <li162002@mail.ru>
2021-04-18 15:00:54 +0300

    edit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="ap" >
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
+<script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  name: 'App',
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+</script>
+<style lang="scss">
+.ap{
+  width:100%; 
+  height:100%;
+  top:0;
+  left:0;
+  position:fixed;
+  color:white;
+  background-image: url('assets/fon.png');
 }
 </style>
