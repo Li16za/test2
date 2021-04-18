1db6c7f7b35101a6405ca843aeac6a2e0592db2f
user <li162002@mail.ru>
2021-04-18 22:11:01 +0300

    edit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,39 +13,29 @@ import todolist from '@/views/todolist'
 export default {
   name: 'Home',
   components:{
-   todolist
+    todolist
   },
-    data(){
+  data(){
     return {
       todos:[]
-    }},
-    mounted(){
+  }},
+  mounted(){
       axios.get('https://api.le-systeme-solaire.net/rest/bodies/')
       .then(response=>this.todos=response.data.bodies)
       .catch(error=>{console.error(error);});
-},
-methods:{
-}}
+  }
+}
 </script>
 <style>
 td{
   width: 250px;
   text-align:center;
-  height:50%;
+  height:50px;
   background:black;
   border:2px solid rgb(235, 216, 216);
-
-}
-table{
-  margin:auto;
-  text-align: center;
-  color:white;
-}
-h1{
-  color:white;
-  text-align:center;
 }
 div{
   color:white;
+  text-align:center;
 }
 </style>
