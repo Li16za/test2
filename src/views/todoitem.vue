42e5af41e618ccfe07150284d86b897b45dedf68
user <li162002@mail.ru>
2021-04-20 23:07:02 +0300

    edit

--- src/views/todoitem.vue
+++ src/views/todoitem.vue
@@ -4,6 +4,40 @@
     <td>{{todo.isPlanet}}</td>
     <td>{{todo.discoveryDate}} {{todo.discoveredBy}}
     </td>
+    
+        <div class="load">
+        <div class="form-group">
+            <button class="btn btn-primary" @click.prevent="showModalTwo = !showModalTwo">Update</button>
+            <button class="btn btn-primary" @click.prevent="showModalTree = !showModalTree">Delete</button>
+        </div>
+        <div class="customModal" v-if="showModalTwo">
+    <div class="customModalTitle">
+        Update
+    </div>
+    <div class="customModalBody">
+        <p>Содержание окна</p>
+    </div>
+    <div class="customModalFooter">
+        <button class="btn btn-primary" @click.prevent="showModalTwo= !showModalTwo;/*edi*/">Update</button>
+        <button class="close" @click.prevent="showModalTwo = !showModalTwo">&times;</button>
+    
+    </div>
+</div>
+        <div class="customModal" v-if="showModalTree">
+    <div class="customModalTitle">
+DELETE
+
+</div>
+    <div class="customModalBody">
+        <p>Содержание окна</p>
+    </div>
+    <div class="customModalFooter">
+        <button class="btn btn-primary" @click.prevent="$emit('rem-todo',todo.id);showModalTree = !showModalTree;">DELETE</button>
+<button class="close" @click.prevent="showModalTree = !showModalTree" >&times;</button>    
+    </div>
+</div>
+</div>
+    
 </tr>
 </template>
 <script>
@@ -13,6 +47,11 @@ export default {
             type:Object,
             required:true
         },
-    }
+    },
+    data(){
+        return {
+            showModalTwo: false,
+            showModalTree: false,
+    }}
 }
 </script>
