0a896a1f2b00df9e0162d47e936ea87988d2bf8d
user <li162002@mail.ru>
2021-04-22 00:26:19 +0300

    edit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import getter from './getter'
+import state from './state'
+import action from './action'
+import mutation from './mutation'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state:state,
+  mutations:mutation,
+  actions:action,
+  getters:getter
 })
