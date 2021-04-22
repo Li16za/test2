78265dcf4d66f73567f4428fd2eed0c4bd71df96
user <li162002@mail.ru>
2021-04-22 22:10:47 +0300

    edit

--- src/components/staritem.vue
+++ src/components/staritem.vue
@@ -6,19 +6,31 @@
     </td>
         <div class="load">
         <div class="form-group">
-            <button class="btn btn-primary" @click.prevent="showModalTwo = !showModalTwo">Update</button>
-            <button class="btn btn-primary" @click.prevent="showModalTree = !showModalTree">Delete</button>
+            <button class="btn btn-primary" @click="showModalTwo = !showModalTwo">Update</button>
+            <button class="btn btn-primary" @click="showModalTree = !showModalTree">Delete</button>
+            <button class="btn btn-primary" @click="$router.push({ path: `/Info/${star.id}` })"> подробнее </button>
         </div>
         <div class="customModal" v-if="showModalTwo">
     <div class="customModalTitle">
         Update
     </div>
     <div class="customModalBody">
-        <p>Содержание окна</p>
+            Небесное тело: <br>
+            <input type="text" v-model="name" class="input" >
+            <br> Является ли объект планетой: <br>
+            <select name="isPlanet" v-model="isPlanet" class="input" >
+                <option value="true">true</option>
+                <option value="false">false</option>
+            </select>
+            <br> Дата открытия: <br>
+            <input type="text" v-model="discoveryDate" class="input">
+            <br>Кто открыл: <br>
+            <input type="text" v-model="discoveredBy" class="input" >
+            <br>
     </div>
     <div class="customModalFooter">
-        <button class="btn btn-primary" @click.prevent="showModalTwo= !showModalTwo;/*edi*/">Update</button>
-        <button class="close" @click.prevent="showModalTwo = !showModalTwo">&times;</button>
+        <button class="btn btn-primary" @click="editStar">Update</button>
+        <button class="close" @click="showModalTwo = !showModalTwo">&times;</button>
     
     </div>
 </div>
@@ -52,13 +64,29 @@ export default {
         return {
             showModalTwo: false,
             showModalTree: false,
-    }
+            name:this.star.name,
+            isPlanet:this.star.isPlanet,
+            discoveryDate:this.star.discoveryDate,
+            discoveredBy:this.star.discoveredBy,
+            id:this.star.id,
+        }
     },
     methods:{
-            ...mapMutations(['DeleteStars']),
+            ...mapMutations(['DeleteStars','UpdateStars']),
             deleteStar(){
                 this.DeleteStars(this.star.id);
                 this.showModalTree = !this.showModalTree;
+            },
+            editStar(){
+                var star={
+                    'id':this.id,
+                    'name':this.name,
+                    'isPlanet':this.isPlanet,
+                    'discoveryDate':this.discoveryDate,
+                    'discoveredBy':this.discoveredBy,
+                };
+                this.UpdateStars(star);
+                this.showModalTwo = !this.showModalTwo;
             }
     }
 }
