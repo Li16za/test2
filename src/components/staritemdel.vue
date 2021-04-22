bfc57122a1f26aac6df114e644a0d9653ca166b0
user <li162002@mail.ru>
2021-04-22 22:06:05 +0300

    new

---
+++ src/components/staritemdel.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <tr>
+    <td>{{stari.name}}</td>
+    <td>{{stari.isPlanet}}</td>
+    <td>{{stari.discoveryDate}} {{stari.discoveredBy}}
+    </td>
+    </tr>
+</template>
+<script>
+export default {
+    props: {
+        stari:{
+            type:Object,
+            required:true
+        }
+    }
+}
+</script>
